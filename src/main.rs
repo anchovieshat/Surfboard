9bb402833023c50de38426aaa9db3c003859c2ec
Colin Davidson <colin@pentaquine.com>
2016-01-09 00:20:38 -0800

    Writing found to be broken, took out serialization in preparation for redo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 extern crate byteorder;
-extern crate rustc_serialize;
-extern crate bincode;
 
 use std::str;
 use std::fs::File;
@@ -9,7 +7,6 @@ use std::io::prelude::*;
 use std::env;
 use byteorder::{LittleEndian, BigEndian, ByteOrder, ReadBytesExt, WriteBytesExt};
 
-#[derive(RustcEncodable)]
 struct Fmt {
     id: u32,
     size: u32,
@@ -22,23 +19,12 @@ struct Fmt {
 }
 
 impl Fmt {
-    fn new(num_channels: u16, sample_rate: u32, bits_per_sample: u16) -> Fmt {
+    fn new(num_channels: u16, sample_rate: u32, bits_per_sample: u16) {
         let id = BigEndian::read_u32(b"fmt ");
         let size = 16; //PCM format size
         let audio_fmt = 1; //Linear Quantization
         let byte_rate = (sample_rate * (num_channels as u32) * (bits_per_sample as u32)) / 8;
         let block_align = (num_channels * bits_per_sample) / 8;
-
-        Fmt {
-            id: id,
-            size: size,
-            audio_fmt: audio_fmt,
-            num_channels: num_channels,
-            sample_rate: sample_rate,
-            byte_rate: byte_rate,
-            block_align: block_align,
-            bits_per_sample: bits_per_sample,
-        }
     }
 
     fn parse<R: io::Read>(r: &mut R) -> Fmt {
@@ -77,7 +63,6 @@ impl Fmt {
     }
 }
 
-#[derive(RustcEncodable)]
 struct Data {
     id: u32,
     size: u32,
@@ -85,16 +70,11 @@ struct Data {
 }
 
 impl Data {
-    fn new(data: Vec<u8>) -> Data {
+    fn new(data: Vec<u8>) {
         let id = BigEndian::read_u32(b"data");
-        Data {
-            id: id,
-            size: data.len() as u32,
-            data: data,
-        }
     }
 
-    fn parse<R: io::Read>(r: &mut R) {
+    fn parse<R: io::Read>(r: &mut R) -> Data {
         let id = r.read_u32::<LittleEndian>().unwrap();
         let size = r.read_u32::<LittleEndian>().unwrap();
         let mut data = Vec::new();
@@ -107,6 +87,11 @@ impl Data {
 
         println!("data size: {}", size);
 
+        Data {
+            id: id,
+            size: size,
+            data: data,
+        }
     }
 }
 
@@ -114,15 +99,16 @@ struct List {
     list_id: u32,
     size: u32,
     type_id: u32,
+    data: Vec<u8>,
 }
 
 impl List {
-    fn parse<R: io::Read>(r: &mut R) {
+    fn parse<R: io::Read + io::Seek>(r: &mut R) -> List {
         let list_id = r.read_u32::<LittleEndian>().unwrap();
         let size = r.read_u32::<LittleEndian>().unwrap();
         let type_id = r.read_u32::<LittleEndian>().unwrap();
-        let new_id = r.read_u32::<LittleEndian>().unwrap();
-
+        let data = Vec::new();
+        r.seek(io::SeekFrom::Current((size - 4) as i64)).unwrap();
 
         let mut t = vec![];
         t.write_u32::<LittleEndian>(list_id).unwrap();
@@ -134,10 +120,16 @@ impl List {
         t.write_u32::<LittleEndian>(type_id).unwrap();
         println!("type id: {}", str::from_utf8(&t).unwrap());
         t.clear();
+
+        List {
+            list_id: list_id,
+            size: size,
+            type_id: type_id,
+            data: data,
+        }
     }
 }
 
-#[derive(RustcEncodable)]
 struct Wave {
     chunk_id: u32,
     chunk_size: u32,
@@ -147,23 +139,15 @@ struct Wave {
 }
 
 impl Wave {
-    fn new(num_channels: u16, sample_rate: u32, bits_per_sample: u16, data: Vec<u8>) -> Wave {
+    fn write(num_channels: u16, sample_rate: u32, bits_per_sample: u16, data: Vec<u8>) {
         let chunk_id = BigEndian::read_u32(b"RIFF");
         let format = BigEndian::read_u32(b"WAVE");
         let fmt_chunk = Fmt::new(num_channels, sample_rate, bits_per_sample);
         let data_chunk = Data::new(data);
         let chunk_size = 4 + (8 + fmt_chunk.size) + (8 + data_chunk.size); // Filesize - (chunk_id + chunk_size)
-
-        Wave {
-            chunk_id: chunk_id,
-            chunk_size: chunk_size as u32,
-            format: format,
-            fmt: fmt_chunk,
-            data: data_chunk,
-        }
     }
 
-    fn parse<R: io::Read + io::Seek>(r: &mut R) {
+    fn parse<R: io::Read + io::Seek>(r: &mut R) -> Wave {
         r.seek(io::SeekFrom::Start(0)).unwrap();
         let chunk_id = r.read_u32::<LittleEndian>().unwrap();
         let chunk_size = r.read_u32::<LittleEndian>().unwrap();
@@ -181,26 +165,28 @@ impl Wave {
         t.clear();
 
         let fmt_chunk = Fmt::parse(r);
-        let list_chunk = List::parse(r);
+        //let list_chunk = List::parse(r);
+        let data_chunk = Data::parse(r);
 
-        /*Wave {
+        Wave {
             chunk_id: chunk_id,
             chunk_size: chunk_size,
             format: format,
             fmt: fmt_chunk,
             data: data_chunk,
-        }*/
+        }
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //let mut wav_file = File::create(&args[1]).unwrap();
-    let mut base_file = File::open(&args[1]).unwrap();
+    {
+        let mut wav_file = File::create(&args[1]).unwrap();
 
-    let wav = Wave::parse(&mut base_file);
+        let data = Vec::new();
+        Wave::write(1, 44100, 8, data);
+    }
 
-    //let wav = Wave::new(1, 44100, 8, data);
-
-    //bincode::rustc_serialize::encode_into(&wav, &mut wav_file, bincode::SizeLimit::Infinite).unwrap();
+    let mut read_test = File::open(&args[1]).unwrap();
+    let real_wav = Wave::parse(&mut read_test);
 }
